16f1d64571bedbd6a8f6924a0c7e8e87d061284a
Jesse <jesse_shrt@yahoo.com>
2018-04-30 22:15:04 -0400

    working on header. created mobile menu toggle and hamburger to x animation

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,11 +5,12 @@ const   gulp        = require( "gulp" ),
         cssVars     = require( "postcss-simple-vars" ),
         nested      = require( "postcss-nested" ),
         cssImp      = require( "postcss-import" ),
-        mixins      = require( "postcss-mixins");
+        mixins      = require( "postcss-mixins"),
+        hexrgba     = require( "postcss-hexrgba");
         
 gulp.task("styles", function(){
     return gulp.src('./app/assets/styles/styles.css')
-    .pipe(pCSS([cssImp, mixins, nested, cssVars, autoPre]))
+    .pipe(pCSS([cssImp, mixins, cssVars, nested, hexrgba, autoPre]))
     .on('error', function( errorInfo ){
         console.error( errorInfo.toString() );
         this.emit('end');
